89ec6a63441ba6997d0ee20d043a5341e2c9feec
Ander Hernandez <anderhernandez066@gmail.com>
2026-06-28 12:51:40 -0600

    Agregar archivos de proyecto.

---
+++ pry.FormulariosMeetHub/frmLogin.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace pry.FormulariosMeetHub
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            lblLogin = new Label();
+            pcbLogo = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pcbLogo).BeginInit();
+            SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Font = new Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblLogin.ForeColor = Color.FromArgb(63, 95, 132);
+            lblLogin.Location = new Point(199, 31);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(135, 50);
+            lblLogin.TabIndex = 0;
+            lblLogin.Text = "LOGIN";
+            // 
+            // pcbLogo
+            // 
+            pcbLogo.Image = (Image)resources.GetObject("pcbLogo.Image");
+            pcbLogo.Location = new Point(443, 12);
+            pcbLogo.Name = "pcbLogo";
+            pcbLogo.Size = new Size(84, 80);
+            pcbLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbLogo.TabIndex = 1;
+            pcbLogo.TabStop = false;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(539, 450);
+            Controls.Add(pcbLogo);
+            Controls.Add(lblLogin);
+            Name = "frmLogin";
+            Text = "Control de Acceso";
+            ((System.ComponentModel.ISupportInitialize)pcbLogo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblLogin;
+        private PictureBox pcbLogo;
+    }
+}
---
+++ pry.FormulariosMeetHub/frmLogin.cs
@@ -0,0 +1,10 @@
+namespace pry.FormulariosMeetHub
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
