258656ce0b74f0075b45d7194eee652a7b0a7bfd
laryb <laryb@LARY>
2026-07-01 08:18:48 -0600

    cambie tamaños

--- pry.FormulariosMeetHub/apartado de reservaciones.Designer.cs
+++ pry.FormulariosMeetHub/apartado de reservaciones.Designer.cs
@@ -58,9 +58,10 @@
             TextBox1.BackColor = Color.FromArgb(231, 238, 246);
             TextBox1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             TextBox1.ForeColor = Color.FromArgb(63, 95, 132);
-            TextBox1.Location = new Point(42, 17);
+            TextBox1.Location = new Point(60, 28);
+            TextBox1.Margin = new Padding(4, 5, 4, 5);
             TextBox1.Name = "TextBox1";
-            TextBox1.Size = new Size(293, 27);
+            TextBox1.Size = new Size(417, 37);
             TextBox1.TabIndex = 1;
             TextBox1.Text = "  Responsable de la Reserva";
             // 
@@ -70,10 +71,11 @@
             ComboBox1.Font = new Font("Segoe UI", 11.25F);
             ComboBox1.ForeColor = Color.FromArgb(63, 95, 132);
             ComboBox1.FormattingEnabled = true;
-            ComboBox1.ItemHeight = 20;
-            ComboBox1.Location = new Point(42, 50);
+            ComboBox1.ItemHeight = 31;
+            ComboBox1.Location = new Point(60, 83);
+            ComboBox1.Margin = new Padding(4, 5, 4, 5);
             ComboBox1.Name = "ComboBox1";
-            ComboBox1.Size = new Size(293, 28);
+            ComboBox1.Size = new Size(417, 39);
             ComboBox1.TabIndex = 2;
             ComboBox1.Text = "Sala:";
             // 
@@ -82,9 +84,10 @@
             Label1.AutoSize = true;
             Label1.Font = new Font("Segoe UI", 11.25F);
             Label1.ForeColor = Color.FromArgb(63, 95, 132);
-            Label1.Location = new Point(42, 81);
+            Label1.Location = new Point(60, 135);
+            Label1.Margin = new Padding(4, 0, 4, 0);
             Label1.Name = "Label1";
-            Label1.Size = new Size(98, 20);
+            Label1.Size = new Size(149, 31);
             Label1.TabIndex = 3;
             Label1.Text = "Detalles Sala:";
             // 
@@ -94,9 +97,10 @@
             ComboBox2.Font = new Font("Segoe UI", 11.25F);
             ComboBox2.ForeColor = Color.FromArgb(63, 95, 132);
             ComboBox2.FormattingEnabled = true;
-            ComboBox2.Location = new Point(42, 121);
+            ComboBox2.Location = new Point(60, 202);
+            ComboBox2.Margin = new Padding(4, 5, 4, 5);
             ComboBox2.Name = "ComboBox2";
-            ComboBox2.Size = new Size(293, 28);
+            ComboBox2.Size = new Size(417, 39);
             ComboBox2.TabIndex = 5;
             ComboBox2.Text = "Àrea Academica/Departamento";
             // 
@@ -105,9 +109,10 @@
             Label2.AutoSize = true;
             Label2.Font = new Font("Segoe UI", 11.25F);
             Label2.ForeColor = Color.FromArgb(63, 95, 132);
-            Label2.Location = new Point(42, 160);
+            Label2.Location = new Point(60, 267);
+            Label2.Margin = new Padding(4, 0, 4, 0);
             Label2.Name = "Label2";
-            Label2.Size = new Size(125, 20);
+            Label2.Size = new Size(191, 31);
             Label2.TabIndex = 6;
             Label2.Text = "Fecha De Reserva";
             // 
@@ -116,9 +121,10 @@
             DateTimePicker1.CalendarForeColor = Color.FromArgb(63, 95, 132);
             DateTimePicker1.CalendarTitleBackColor = Color.FromArgb(231, 238, 246);
             DateTimePicker1.Font = new Font("Segoe UI", 11.25F);
-            DateTimePicker1.Location = new Point(219, 155);
+            DateTimePicker1.Location = new Point(313, 258);
+            DateTimePicker1.Margin = new Padding(4, 5, 4, 5);
             DateTimePicker1.Name = "DateTimePicker1";
-            DateTimePicker1.Size = new Size(263, 27);
+            DateTimePicker1.Size = new Size(374, 37);
             DateTimePicker1.TabIndex = 7;
             // 
             // label3
@@ -126,18 +132,20 @@
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 11F);
             label3.ForeColor = Color.FromArgb(63, 95, 132);
-            label3.Location = new Point(42, 198);
+            label3.Location = new Point(60, 330);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(167, 20);
+            label3.Size = new Size(241, 30);
             label3.TabIndex = 8;
             label3.Text = "Fecha De Formalización";
             // 
             // dateTimePicker2
             // 
             dateTimePicker2.Font = new Font("Segoe UI", 11.25F);
-            dateTimePicker2.Location = new Point(219, 191);
+            dateTimePicker2.Location = new Point(313, 318);
+            dateTimePicker2.Margin = new Padding(4, 5, 4, 5);
             dateTimePicker2.Name = "dateTimePicker2";
-            dateTimePicker2.Size = new Size(263, 27);
+            dateTimePicker2.Size = new Size(374, 37);
             dateTimePicker2.TabIndex = 9;
             // 
             // ComboBox3
@@ -146,9 +154,10 @@
             ComboBox3.Font = new Font("Segoe UI", 11.25F);
             ComboBox3.ForeColor = Color.FromArgb(63, 95, 132);
             ComboBox3.FormattingEnabled = true;
-            ComboBox3.Location = new Point(56, 248);
+            ComboBox3.Location = new Point(80, 413);
+            ComboBox3.Margin = new Padding(4, 5, 4, 5);
             ComboBox3.Name = "ComboBox3";
-            ComboBox3.Size = new Size(98, 28);
+            ComboBox3.Size = new Size(138, 39);
             ComboBox3.TabIndex = 10;
             ComboBox3.Text = "Hora Inicio:";
             // 
@@ -158,9 +167,10 @@
             ComboBox4.Font = new Font("Segoe UI", 11.25F);
             ComboBox4.ForeColor = Color.FromArgb(63, 95, 132);
             ComboBox4.FormattingEnabled = true;
-            ComboBox4.Location = new Point(220, 248);
+            ComboBox4.Location = new Point(314, 413);
+            ComboBox4.Margin = new Padding(4, 5, 4, 5);
             ComboBox4.Name = "ComboBox4";
-            ComboBox4.Size = new Size(115, 28);
+            ComboBox4.Size = new Size(163, 39);
             ComboBox4.TabIndex = 11;
             ComboBox4.Text = "Hora Termino:";
             // 
@@ -170,9 +180,10 @@
             ComboBox5.Font = new Font("Segoe UI", 11.25F);
             ComboBox5.ForeColor = Color.FromArgb(63, 95, 132);
             ComboBox5.FormattingEnabled = true;
-            ComboBox5.Location = new Point(42, 294);
+            ComboBox5.Location = new Point(60, 490);
+            ComboBox5.Margin = new Padding(4, 5, 4, 5);
             ComboBox5.Name = "ComboBox5";
-            ComboBox5.Size = new Size(293, 28);
+            ComboBox5.Size = new Size(417, 39);
             ComboBox5.TabIndex = 12;
             ComboBox5.Text = "Tipo de Evento:";
             ComboBox5.UseWaitCursor = true;
@@ -182,9 +193,10 @@
             TextBox2.BackColor = Color.FromArgb(231, 238, 246);
             TextBox2.Font = new Font("Segoe UI", 11.25F);
             TextBox2.ForeColor = Color.FromArgb(63, 95, 132);
-            TextBox2.Location = new Point(42, 328);
+            TextBox2.Location = new Point(60, 547);
+            TextBox2.Margin = new Padding(4, 5, 4, 5);
             TextBox2.Name = "TextBox2";
-            TextBox2.Size = new Size(293, 27);
+            TextBox2.Size = new Size(417, 37);
             TextBox2.TabIndex = 13;
             TextBox2.Text = "Descripciòn del evento";
             // 
@@ -194,9 +206,10 @@
             ComboBox6.Font = new Font("Segoe UI", 11.25F);
             ComboBox6.ForeColor = Color.FromArgb(63, 95, 132);
             ComboBox6.FormattingEnabled = true;
-            ComboBox6.Location = new Point(42, 371);
+            ComboBox6.Location = new Point(60, 618);
+            ComboBox6.Margin = new Padding(4, 5, 4, 5);
             ComboBox6.Name = "ComboBox6";
-            ComboBox6.Size = new Size(293, 28);
+            ComboBox6.Size = new Size(417, 39);
             ComboBox6.TabIndex = 14;
             ComboBox6.Text = "Total de Asistentes:";
             // 
@@ -205,9 +218,10 @@
             Button1.BackColor = Color.FromArgb(231, 238, 246);
             Button1.Font = new Font("Arial", 11F);
             Button1.ForeColor = Color.FromArgb(63, 95, 132);
-            Button1.Location = new Point(73, 405);
+            Button1.Location = new Point(104, 675);
+            Button1.Margin = new Padding(4, 5, 4, 5);
             Button1.Name = "Button1";
-            Button1.Size = new Size(94, 33);
+            Button1.Size = new Size(134, 55);
             Button1.TabIndex = 15;
             Button1.Text = "Aceptar";
             Button1.UseVisualStyleBackColor = false;
@@ -217,9 +231,10 @@
             Button2.BackColor = Color.FromArgb(231, 238, 246);
             Button2.Font = new Font("Arial", 11F);
             Button2.ForeColor = Color.FromArgb(63, 95, 132);
-            Button2.Location = new Point(203, 405);
+            Button2.Location = new Point(290, 675);
+            Button2.Margin = new Padding(4, 5, 4, 5);
             Button2.Name = "Button2";
-            Button2.Size = new Size(94, 33);
+            Button2.Size = new Size(134, 55);
             Button2.TabIndex = 16;
             Button2.Text = "Cancelar";
             Button2.UseVisualStyleBackColor = false;
@@ -229,9 +244,10 @@
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 11.25F);
             label4.ForeColor = Color.FromArgb(63, 95, 132);
-            label4.Location = new Point(498, 50);
+            label4.Location = new Point(711, 83);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(42, 20);
+            label4.Size = new Size(64, 31);
             label4.TabIndex = 17;
             label4.Text = "Nota";
             // 
@@ -239,9 +255,10 @@
             // 
             pictureBox3.BorderStyle = BorderStyle.FixedSingle;
             pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(693, -7);
+            pictureBox3.Location = new Point(959, 14);
+            pictureBox3.Margin = new Padding(4, 5, 4, 5);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(120, 120);
+            pictureBox3.Size = new Size(171, 199);
             pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox3.TabIndex = 18;
             pictureBox3.TabStop = false;
@@ -250,9 +267,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(498, 142);
+            pictureBox1.Location = new Point(711, 237);
+            pictureBox1.Margin = new Padding(4, 5, 4, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(32, 38);
+            pictureBox1.Size = new Size(46, 63);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 19;
             pictureBox1.TabStop = false;
@@ -260,19 +278,20 @@
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(488, 188);
+            pictureBox2.Location = new Point(697, 313);
+            pictureBox2.Margin = new Padding(4, 5, 4, 5);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(82, 30);
+            pictureBox2.Size = new Size(117, 50);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 20;
             pictureBox2.TabStop = false;
             // 
             // apartado_de_reservaciones
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1143, 750);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
             Controls.Add(pictureBox3);
@@ -292,6 +311,7 @@
             Controls.Add(Label1);
             Controls.Add(ComboBox1);
             Controls.Add(TextBox1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "apartado_de_reservaciones";
             Text = "apartado_de_reservaciones";
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
--- pry.FormulariosMeetHub/frmLogin.Designer.cs
+++ pry.FormulariosMeetHub/frmLogin.Designer.cs
@@ -36,11 +36,9 @@
             button1 = new Button();
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
-            pictureBox3 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pcbLogo).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // lblLogin
@@ -48,18 +46,20 @@
             lblLogin.AutoSize = true;
             lblLogin.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
             lblLogin.ForeColor = Color.FromArgb(63, 95, 132);
-            lblLogin.Location = new Point(267, 99);
+            lblLogin.Location = new Point(381, 165);
+            lblLogin.Margin = new Padding(4, 0, 4, 0);
             lblLogin.Name = "lblLogin";
-            lblLogin.Size = new Size(80, 30);
+            lblLogin.Size = new Size(117, 45);
             lblLogin.TabIndex = 0;
             lblLogin.Text = "LOGIN";
             // 
             // pcbLogo
             // 
             pcbLogo.Image = (Image)resources.GetObject("pcbLogo.Image");
-            pcbLogo.Location = new Point(716, 1);
+            pcbLogo.Location = new Point(1023, 2);
+            pcbLogo.Margin = new Padding(4, 5, 4, 5);
             pcbLogo.Name = "pcbLogo";
-            pcbLogo.Size = new Size(89, 67);
+            pcbLogo.Size = new Size(127, 112);
             pcbLogo.SizeMode = PictureBoxSizeMode.StretchImage;
             pcbLogo.TabIndex = 1;
             pcbLogo.TabStop = false;
@@ -68,10 +68,10 @@
             // 
             textBox1.Font = new Font("Arial", 11F);
             textBox1.ForeColor = Color.FromArgb(63, 95, 132);
-            textBox1.Location = new Point(195, 144);
-            textBox1.Margin = new Padding(1);
+            textBox1.Location = new Point(279, 240);
+            textBox1.Margin = new Padding(1, 2, 1, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(250, 24);
+            textBox1.Size = new Size(355, 33);
             textBox1.TabIndex = 11;
             textBox1.Text = "Nombre de usuario";
             // 
@@ -79,10 +79,10 @@
             // 
             textBox2.Font = new Font("Arial", 11F);
             textBox2.ForeColor = Color.FromArgb(63, 95, 132);
-            textBox2.Location = new Point(195, 188);
-            textBox2.Margin = new Padding(1);
+            textBox2.Location = new Point(279, 313);
+            textBox2.Margin = new Padding(1, 2, 1, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(250, 24);
+            textBox2.Size = new Size(355, 33);
             textBox2.TabIndex = 12;
             textBox2.Text = "Contraseña";
             textBox2.TextChanged += textBox2_TextChanged;
@@ -91,10 +91,10 @@
             // 
             button1.BackColor = Color.FromArgb(235, 197, 153);
             button1.Font = new Font("Arial", 11F);
-            button1.Location = new Point(267, 236);
-            button1.Margin = new Padding(1);
+            button1.Location = new Point(381, 393);
+            button1.Margin = new Padding(1, 2, 1, 2);
             button1.Name = "button1";
-            button1.Size = new Size(80, 26);
+            button1.Size = new Size(114, 43);
             button1.TabIndex = 13;
             button1.Text = "Ingresar";
             button1.UseVisualStyleBackColor = false;
@@ -102,9 +102,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(128, 188);
+            pictureBox1.Location = new Point(183, 313);
+            pictureBox1.Margin = new Padding(4, 5, 4, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(40, 40);
+            pictureBox1.Size = new Size(57, 67);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 14;
             pictureBox1.TabStop = false;
@@ -112,31 +113,20 @@
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(128, 128);
+            pictureBox2.Location = new Point(183, 213);
+            pictureBox2.Margin = new Padding(4, 5, 4, 5);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(40, 40);
+            pictureBox2.Size = new Size(57, 67);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 15;
             pictureBox2.TabStop = false;
             // 
-            // pictureBox3
-            // 
-            pictureBox3.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(488, -17);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(120, 120);
-            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox3.TabIndex = 16;
-            pictureBox3.TabStop = false;
-            // 
             // frmLogin
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(584, 361);
-            Controls.Add(pictureBox3);
+            ClientSize = new Size(1143, 750);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
             Controls.Add(button1);
@@ -144,12 +134,12 @@
             Controls.Add(textBox1);
             Controls.Add(pcbLogo);
             Controls.Add(lblLogin);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "frmLogin";
             Text = "Control de acceso - Meet Hub";
             ((System.ComponentModel.ISupportInitialize)pcbLogo).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -163,6 +153,5 @@
         private Button button1;
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
-        private PictureBox pictureBox3;
     }
 }
