29696cb5a886e16b58941a068c1f07df826903c6
Ander Hernandez <anderhernandez066@gmail.com>
2026-07-05 23:19:34 -0600

    Corregi formulario Login

--- pry.FormulariosMeetHub/PantallaAdmin.Designer.cs
+++ pry.FormulariosMeetHub/PantallaAdmin.Designer.cs
@@ -95,7 +95,7 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(693, -24);
+            pictureBox1.Location = new Point(604, 103);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(120, 120);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
--- pry.FormulariosMeetHub/Properties/Resources.Designer.cs
+++ pry.FormulariosMeetHub/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace pry.FormulariosMeetHub.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap IconoContraseña_removebg_preview {
+            get {
+                object obj = ResourceManager.GetObject("IconoContraseña-removebg-preview", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap IconoLogin_removebg_preview {
+            get {
+                object obj = ResourceManager.GetObject("IconoLogin-removebg-preview", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
