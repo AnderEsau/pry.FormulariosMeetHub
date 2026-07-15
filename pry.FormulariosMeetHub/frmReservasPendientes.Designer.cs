7bfc210436beaac9bcd9f663fe9f3e176e4c5226
laryb <laryb@LARY>
2026-07-14 23:21:46 -0600

    Realice el codigo del formulario de reservas pendientes

--- pry.FormulariosMeetHub/frmReservasPendientes.Designer.cs
+++ pry.FormulariosMeetHub/frmReservasPendientes.Designer.cs
@@ -28,19 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             lblTitulo = new Label();
             pnlReservacionesP = new Panel();
             btnCancelar = new Button();
             btnAceptar = new Button();
             grpDetalles = new GroupBox();
+            txtMotivo = new TextBox();
+            txtDescripcion = new TextBox();
+            txtTotalAsistentes = new TextBox();
+            txtTipoEvento = new TextBox();
+            txtHoraFin = new TextBox();
+            txtHoraInicio = new TextBox();
             dgvReservasPendientes = new DataGridView();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            Column3 = new DataGridViewTextBoxColumn();
             pnlReservacionesP.SuspendLayout();
+            grpDetalles.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvReservasPendientes).BeginInit();
             SuspendLayout();
             // 
@@ -50,9 +53,10 @@
             lblTitulo.BackColor = Color.Transparent;
             lblTitulo.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblTitulo.ForeColor = Color.FromArgb(43, 62, 80);
-            lblTitulo.Location = new Point(21, 9);
+            lblTitulo.Location = new Point(30, 15);
+            lblTitulo.Margin = new Padding(4, 0, 4, 0);
             lblTitulo.Name = "lblTitulo";
-            lblTitulo.Size = new Size(353, 47);
+            lblTitulo.Size = new Size(537, 71);
             lblTitulo.TabIndex = 74;
             lblTitulo.Text = "Reservas Pendientes";
             // 
@@ -63,9 +67,10 @@
             pnlReservacionesP.Controls.Add(grpDetalles);
             pnlReservacionesP.Controls.Add(dgvReservasPendientes);
             pnlReservacionesP.Dock = DockStyle.Bottom;
-            pnlReservacionesP.Location = new Point(0, 59);
+            pnlReservacionesP.Location = new Point(0, 99);
+            pnlReservacionesP.Margin = new Padding(4, 5, 4, 5);
             pnlReservacionesP.Name = "pnlReservacionesP";
-            pnlReservacionesP.Size = new Size(1119, 497);
+            pnlReservacionesP.Size = new Size(1599, 828);
             pnlReservacionesP.TabIndex = 75;
             // 
             // btnCancelar
@@ -76,10 +81,10 @@
             btnCancelar.FlatStyle = FlatStyle.Flat;
             btnCancelar.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnCancelar.ForeColor = Color.FromArgb(42, 36, 33);
-            btnCancelar.Location = new Point(643, 431);
-            btnCancelar.Margin = new Padding(1);
+            btnCancelar.Location = new Point(919, 718);
+            btnCancelar.Margin = new Padding(1, 2, 1, 2);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(112, 33);
+            btnCancelar.Size = new Size(160, 55);
             btnCancelar.TabIndex = 37;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = false;
@@ -92,93 +97,129 @@
             btnAceptar.FlatStyle = FlatStyle.Flat;
             btnAceptar.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnAceptar.ForeColor = Color.White;
-            btnAceptar.Location = new Point(448, 431);
-            btnAceptar.Margin = new Padding(1);
+            btnAceptar.Location = new Point(640, 718);
+            btnAceptar.Margin = new Padding(1, 2, 1, 2);
             btnAceptar.Name = "btnAceptar";
-            btnAceptar.Size = new Size(112, 33);
+            btnAceptar.Size = new Size(160, 55);
             btnAceptar.TabIndex = 36;
-            btnAceptar.Text = "Aceptar";
+            btnAceptar.Text = "Autorizar";
             btnAceptar.UseVisualStyleBackColor = false;
             // 
             // grpDetalles
             // 
             grpDetalles.BackColor = Color.FromArgb(231, 238, 246);
+            grpDetalles.Controls.Add(txtMotivo);
+            grpDetalles.Controls.Add(txtDescripcion);
+            grpDetalles.Controls.Add(txtTotalAsistentes);
+            grpDetalles.Controls.Add(txtTipoEvento);
+            grpDetalles.Controls.Add(txtHoraFin);
+            grpDetalles.Controls.Add(txtHoraInicio);
             grpDetalles.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             grpDetalles.ForeColor = Color.FromArgb(63, 95, 132);
-            grpDetalles.Location = new Point(683, 15);
-            grpDetalles.Margin = new Padding(2);
+            grpDetalles.Location = new Point(976, 25);
             grpDetalles.Name = "grpDetalles";
-            grpDetalles.Padding = new Padding(2);
-            grpDetalles.Size = new Size(366, 356);
+            grpDetalles.Size = new Size(523, 593);
             grpDetalles.TabIndex = 35;
             grpDetalles.TabStop = false;
             grpDetalles.Text = "Detalles de la Reserva:";
             // 
+            // txtMotivo
+            // 
+            txtMotivo.BackColor = SystemColors.ControlLight;
+            txtMotivo.Location = new Point(30, 322);
+            txtMotivo.Multiline = true;
+            txtMotivo.Name = "txtMotivo";
+            txtMotivo.ReadOnly = true;
+            txtMotivo.Size = new Size(462, 49);
+            txtMotivo.TabIndex = 5;
+            // 
+            // txtDescripcion
+            // 
+            txtDescripcion.BackColor = SystemColors.ControlLight;
+            txtDescripcion.Location = new Point(30, 241);
+            txtDescripcion.Multiline = true;
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.ReadOnly = true;
+            txtDescripcion.Size = new Size(462, 49);
+            txtDescripcion.TabIndex = 4;
+            // 
+            // txtTotalAsistentes
+            // 
+            txtTotalAsistentes.BackColor = SystemColors.ControlLight;
+            txtTotalAsistentes.Location = new Point(303, 153);
+            txtTotalAsistentes.Name = "txtTotalAsistentes";
+            txtTotalAsistentes.ReadOnly = true;
+            txtTotalAsistentes.Size = new Size(189, 49);
+            txtTotalAsistentes.TabIndex = 3;
+            // 
+            // txtTipoEvento
+            // 
+            txtTipoEvento.BackColor = SystemColors.ControlLight;
+            txtTipoEvento.Location = new Point(30, 153);
+            txtTipoEvento.Name = "txtTipoEvento";
+            txtTipoEvento.ReadOnly = true;
+            txtTipoEvento.Size = new Size(187, 49);
+            txtTipoEvento.TabIndex = 2;
+            // 
+            // txtHoraFin
+            // 
+            txtHoraFin.BackColor = SystemColors.ControlLight;
+            txtHoraFin.Location = new Point(342, 65);
+            txtHoraFin.Name = "txtHoraFin";
+            txtHoraFin.ReadOnly = true;
+            txtHoraFin.Size = new Size(150, 49);
+            txtHoraFin.TabIndex = 1;
+            // 
+            // txtHoraInicio
+            // 
+            txtHoraInicio.BackColor = SystemColors.ControlLight;
+            txtHoraInicio.Location = new Point(30, 65);
+            txtHoraInicio.Name = "txtHoraInicio";
+            txtHoraInicio.ReadOnly = true;
+            txtHoraInicio.Size = new Size(150, 49);
+            txtHoraInicio.TabIndex = 0;
+            // 
             // dgvReservasPendientes
             // 
             dgvReservasPendientes.BackgroundColor = Color.FromArgb(231, 238, 246);
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle4.BackColor = Color.FromArgb(63, 95, 132);
-            dataGridViewCellStyle4.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle4.ForeColor = Color.White;
-            dataGridViewCellStyle4.SelectionBackColor = Color.FromArgb(43, 62, 80);
-            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
-            dgvReservasPendientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(63, 95, 132);
+            dataGridViewCellStyle1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(43, 62, 80);
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvReservasPendientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             dgvReservasPendientes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvReservasPendientes.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
             dgvReservasPendientes.EnableHeadersVisualStyles = false;
-            dgvReservasPendientes.Location = new Point(57, 15);
-            dgvReservasPendientes.Margin = new Padding(2);
+            dgvReservasPendientes.Location = new Point(54, 25);
             dgvReservasPendientes.Name = "dgvReservasPendientes";
-            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = SystemColors.Control;
-            dataGridViewCellStyle6.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle6.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle6.SelectionBackColor = Color.FromArgb(235, 197, 153);
-            dataGridViewCellStyle6.SelectionForeColor = Color.FromArgb(235, 197, 153);
-            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.True;
-            dgvReservasPendientes.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(235, 197, 153);
+            dataGridViewCellStyle2.SelectionForeColor = Color.FromArgb(235, 197, 153);
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dgvReservasPendientes.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             dgvReservasPendientes.RowHeadersWidth = 62;
-            dgvReservasPendientes.Size = new Size(516, 356);
+            dgvReservasPendientes.Size = new Size(737, 593);
             dgvReservasPendientes.TabIndex = 34;
-            // 
-            // Column1
-            // 
-            dataGridViewCellStyle5.BackColor = Color.White;
-            dataGridViewCellStyle5.ForeColor = Color.FromArgb(43, 62, 80);
-            Column1.DefaultCellStyle = dataGridViewCellStyle5;
-            Column1.HeaderText = "text1";
-            Column1.MinimumWidth = 8;
-            Column1.Name = "Column1";
-            Column1.Width = 150;
-            // 
-            // Column2
-            // 
-            Column2.HeaderText = "text2";
-            Column2.MinimumWidth = 8;
-            Column2.Name = "Column2";
-            Column2.Width = 150;
-            // 
-            // Column3
-            // 
-            Column3.HeaderText = "text3";
-            Column3.MinimumWidth = 8;
-            Column3.Name = "Column3";
-            Column3.Width = 150;
+            dgvReservasPendientes.SelectionChanged += dgvReservasPendientes_SelectionChanged;
             // 
             // frmReservasPendientes
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(240, 236, 230);
-            ClientSize = new Size(1119, 556);
+            ClientSize = new Size(1599, 927);
             Controls.Add(pnlReservacionesP);
             Controls.Add(lblTitulo);
-            Margin = new Padding(2);
             Name = "frmReservasPendientes";
             Text = "Reservas Pendientes";
             pnlReservacionesP.ResumeLayout(false);
+            grpDetalles.ResumeLayout(false);
+            grpDetalles.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dgvReservasPendientes).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -191,8 +232,11 @@
         private Button btnAceptar;
         private GroupBox grpDetalles;
         private DataGridView dgvReservasPendientes;
-        private DataGridViewTextBoxColumn Column1;
-        private DataGridViewTextBoxColumn Column2;
-        private DataGridViewTextBoxColumn Column3;
+        private TextBox txtHoraFin;
+        private TextBox txtHoraInicio;
+        private TextBox txtTotalAsistentes;
+        private TextBox txtTipoEvento;
+        private TextBox txtMotivo;
+        private TextBox txtDescripcion;
     }
 }
--- pry.FormulariosMeetHub/frmReservasPendientes.cs
+++ pry.FormulariosMeetHub/frmReservasPendientes.cs
@@ -12,9 +12,62 @@ namespace pry.FormulariosMeetHub
 {
     public partial class frmReservasPendientes : Form
     {
+        clsReservasPendientes reservas;
+        int idReservaSeleccionada;
+
         public frmReservasPendientes()
         {
             InitializeComponent();
+            CargarGrid();
+        }
+
+        //Metodo para cargar el DataGridView
+        public void CargarGrid()
+        {
+            reservas = new clsReservasPendientes();
+            dgvReservasPendientes.DataSource = null;
+            dgvReservasPendientes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            try
+            {
+                //Asignamos la tabla virtual de la clase directamente al control visual
+                dgvReservasPendientes.DataSource = reservas.CargarDataGrid();
+
+                dgvReservasPendientes.Columns["id_reserva"].Visible = false;
+                dgvReservasPendientes.Columns["hora_inicio"].Visible = false;
+                dgvReservasPendientes.Columns["hora_fin"].Visible = false;
+                dgvReservasPendientes.Columns["tipo_evento"].Visible = false;
+                dgvReservasPendientes.Columns["descripcion_evento"].Visible = false;
+                dgvReservasPendientes.Columns["motivo_evento"].Visible = false;
+                dgvReservasPendientes.Columns["total_asistentes"].Visible = false;
+                dgvReservasPendientes.Columns["estado_reserva"].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dgvReservasPendientes_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validamos que realmente haya una fila seleccionada en el Grid
+                if (dgvReservasPendientes.CurrentRow != null && dgvReservasPendientes.CurrentRow.Cells["id_reserva"].Value != null)
+                {
+                    idReservaSeleccionada = int.Parse(dgvReservasPendientes.CurrentRow.Cells["id_reserva"].Value.ToString());
+                    // esto se mandará a los TextBox
+                    txtHoraInicio.Text = dgvReservasPendientes.CurrentRow.Cells["hora_inicio"].Value.ToString();
+                    txtHoraFin.Text = dgvReservasPendientes.CurrentRow.Cells["hora_fin"].Value.ToString();
+                    txtTipoEvento.Text = dgvReservasPendientes.CurrentRow.Cells["tipo_evento"].Value.ToString();
+                    txtTotalAsistentes.Text = dgvReservasPendientes.CurrentRow.Cells["total_asistentes"].Value.ToString();
+                    txtDescripcion.Text = dgvReservasPendientes.CurrentRow.Cells["descripcion_evento"].Value.ToString();
+                    txtMotivo.Text = dgvReservasPendientes.CurrentRow.Cells["motivo_evento"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los detalles: " + ex.Message);
+            }
         }
     }
 }
