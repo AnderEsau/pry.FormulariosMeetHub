4657dfc05d5dd21306779a74c5e5e16f7a9135b2
laryb <laryb@LARY>
2026-07-16 00:58:41 -0600

    Realice el codigo del formulario de Areas y Departamentos

--- pry.FormulariosMeetHub/frmAreaDpto.Designer.cs
+++ pry.FormulariosMeetHub/frmAreaDpto.Designer.cs
@@ -29,23 +29,19 @@
         private void InitializeComponent()
         {
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAreaDpto));
             lblTitulo = new Label();
             txtNombreArea = new TextBox();
             pnlAreaDpto = new Panel();
             txtDescripcion = new TextBox();
-            txtMatricula = new TextBox();
-            dgvUsuarios = new DataGridView();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            Column3 = new DataGridViewTextBoxColumn();
+            txtArea = new TextBox();
+            dgvAreaDpto = new DataGridView();
             btnEliminar = new Button();
             btnGuardar = new Button();
             btnNuevo = new Button();
             pnlAreaDpto.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvUsuarios).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvAreaDpto).BeginInit();
             SuspendLayout();
             // 
             // lblTitulo
@@ -54,9 +50,10 @@
             lblTitulo.BackColor = Color.Transparent;
             lblTitulo.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblTitulo.ForeColor = Color.FromArgb(43, 62, 80);
-            lblTitulo.Location = new Point(21, 9);
+            lblTitulo.Location = new Point(30, 15);
+            lblTitulo.Margin = new Padding(4, 0, 4, 0);
             lblTitulo.Name = "lblTitulo";
-            lblTitulo.Size = new Size(407, 47);
+            lblTitulo.Size = new Size(616, 71);
             lblTitulo.TabIndex = 77;
             lblTitulo.Text = "Áreas y Departamentos";
             // 
@@ -65,22 +62,24 @@
             txtNombreArea.BackColor = Color.White;
             txtNombreArea.BorderStyle = BorderStyle.FixedSingle;
             txtNombreArea.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtNombreArea.Location = new Point(583, 112);
-            txtNombreArea.Margin = new Padding(1);
+            txtNombreArea.Location = new Point(833, 187);
+            txtNombreArea.Margin = new Padding(1, 2, 1, 2);
             txtNombreArea.Name = "txtNombreArea";
             txtNombreArea.PlaceholderText = "Buscar Área/Departamento";
-            txtNombreArea.Size = new Size(218, 29);
+            txtNombreArea.Size = new Size(311, 39);
             txtNombreArea.TabIndex = 103;
+            txtNombreArea.TextChanged += txtNombreArea_TextChanged;
             // 
             // pnlAreaDpto
             // 
             pnlAreaDpto.BackColor = Color.White;
             pnlAreaDpto.BorderStyle = BorderStyle.FixedSingle;
             pnlAreaDpto.Controls.Add(txtDescripcion);
-            pnlAreaDpto.Controls.Add(txtMatricula);
-            pnlAreaDpto.Location = new Point(64, 144);
+            pnlAreaDpto.Controls.Add(txtArea);
+            pnlAreaDpto.Location = new Point(91, 240);
+            pnlAreaDpto.Margin = new Padding(4, 5, 4, 5);
             pnlAreaDpto.Name = "pnlAreaDpto";
-            pnlAreaDpto.Size = new Size(321, 125);
+            pnlAreaDpto.Size = new Size(458, 207);
             pnlAreaDpto.TabIndex = 102;
             // 
             // txtDescripcion
@@ -88,28 +87,29 @@
             txtDescripcion.BackColor = Color.White;
             txtDescripcion.BorderStyle = BorderStyle.FixedSingle;
             txtDescripcion.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtDescripcion.Location = new Point(21, 72);
-            txtDescripcion.Margin = new Padding(1);
+            txtDescripcion.Location = new Point(30, 120);
+            txtDescripcion.Margin = new Padding(1, 2, 1, 2);
             txtDescripcion.Name = "txtDescripcion";
             txtDescripcion.PlaceholderText = "Descripción";
-            txtDescripcion.Size = new Size(272, 29);
+            txtDescripcion.Size = new Size(388, 39);
             txtDescripcion.TabIndex = 95;
             // 
-            // txtMatricula
+            // txtArea
             // 
-            txtMatricula.BackColor = Color.White;
-            txtMatricula.BorderStyle = BorderStyle.FixedSingle;
-            txtMatricula.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtMatricula.Location = new Point(21, 23);
-            txtMatricula.Margin = new Padding(1);
-            txtMatricula.Name = "txtMatricula";
-            txtMatricula.PlaceholderText = "Nombre del Área/Departamento";
-            txtMatricula.Size = new Size(272, 29);
-            txtMatricula.TabIndex = 94;
+            txtArea.BackColor = Color.White;
+            txtArea.BorderStyle = BorderStyle.FixedSingle;
+            txtArea.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtArea.Location = new Point(30, 38);
+            txtArea.Margin = new Padding(1, 2, 1, 2);
+            txtArea.Name = "txtArea";
+            txtArea.PlaceholderText = "Nombre del Área/Departamento";
+            txtArea.Size = new Size(388, 39);
+            txtArea.TabIndex = 94;
             // 
-            // dgvUsuarios
+            // dgvAreaDpto
             // 
-            dgvUsuarios.BackgroundColor = Color.FromArgb(231, 238, 246);
+            dgvAreaDpto.AllowUserToAddRows = false;
+            dgvAreaDpto.BackgroundColor = Color.FromArgb(231, 238, 246);
             dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle1.BackColor = Color.FromArgb(63, 95, 132);
             dataGridViewCellStyle1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
@@ -117,48 +117,23 @@
             dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(43, 62, 80);
             dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            dgvUsuarios.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            dgvUsuarios.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvUsuarios.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
-            dgvUsuarios.EnableHeadersVisualStyles = false;
-            dgvUsuarios.Location = new Point(583, 144);
-            dgvUsuarios.Margin = new Padding(2);
-            dgvUsuarios.Name = "dgvUsuarios";
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = SystemColors.Control;
-            dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(235, 197, 153);
-            dataGridViewCellStyle3.SelectionForeColor = Color.FromArgb(235, 197, 153);
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
-            dgvUsuarios.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
-            dgvUsuarios.RowHeadersWidth = 62;
-            dgvUsuarios.Size = new Size(510, 223);
-            dgvUsuarios.TabIndex = 101;
-            // 
-            // Column1
-            // 
-            dataGridViewCellStyle2.BackColor = Color.White;
-            dataGridViewCellStyle2.ForeColor = Color.FromArgb(43, 62, 80);
-            Column1.DefaultCellStyle = dataGridViewCellStyle2;
-            Column1.HeaderText = "text1";
-            Column1.MinimumWidth = 8;
-            Column1.Name = "Column1";
-            Column1.Width = 150;
-            // 
-            // Column2
-            // 
-            Column2.HeaderText = "text2";
-            Column2.MinimumWidth = 8;
-            Column2.Name = "Column2";
-            Column2.Width = 150;
-            // 
-            // Column3
-            // 
-            Column3.HeaderText = "text3";
-            Column3.MinimumWidth = 8;
-            Column3.Name = "Column3";
-            Column3.Width = 150;
+            dgvAreaDpto.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgvAreaDpto.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAreaDpto.EnableHeadersVisualStyles = false;
+            dgvAreaDpto.Location = new Point(833, 240);
+            dgvAreaDpto.Name = "dgvAreaDpto";
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(235, 197, 153);
+            dataGridViewCellStyle2.SelectionForeColor = Color.FromArgb(235, 197, 153);
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dgvAreaDpto.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dgvAreaDpto.RowHeadersWidth = 62;
+            dgvAreaDpto.Size = new Size(729, 372);
+            dgvAreaDpto.TabIndex = 101;
+            dgvAreaDpto.SelectionChanged += dgvAreaDpto_SelectionChanged;
             // 
             // btnEliminar
             // 
@@ -167,11 +142,13 @@
             btnEliminar.Cursor = Cursors.Hand;
             btnEliminar.FlatAppearance.BorderSize = 0;
             btnEliminar.FlatStyle = FlatStyle.Flat;
-            btnEliminar.Location = new Point(285, 284);
+            btnEliminar.Location = new Point(407, 473);
+            btnEliminar.Margin = new Padding(4, 5, 4, 5);
             btnEliminar.Name = "btnEliminar";
-            btnEliminar.Size = new Size(101, 48);
+            btnEliminar.Size = new Size(144, 80);
             btnEliminar.TabIndex = 106;
             btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click_1;
             // 
             // btnGuardar
             // 
@@ -180,11 +157,13 @@
             btnGuardar.Cursor = Cursors.Hand;
             btnGuardar.FlatAppearance.BorderSize = 0;
             btnGuardar.FlatStyle = FlatStyle.Flat;
-            btnGuardar.Location = new Point(174, 284);
+            btnGuardar.Location = new Point(249, 473);
+            btnGuardar.Margin = new Padding(4, 5, 4, 5);
             btnGuardar.Name = "btnGuardar";
-            btnGuardar.Size = new Size(101, 48);
+            btnGuardar.Size = new Size(144, 80);
             btnGuardar.TabIndex = 105;
             btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
             // 
             // btnNuevo
             // 
@@ -193,31 +172,34 @@
             btnNuevo.Cursor = Cursors.Hand;
             btnNuevo.FlatAppearance.BorderSize = 0;
             btnNuevo.FlatStyle = FlatStyle.Flat;
-            btnNuevo.Location = new Point(63, 284);
+            btnNuevo.Location = new Point(90, 473);
+            btnNuevo.Margin = new Padding(4, 5, 4, 5);
             btnNuevo.Name = "btnNuevo";
-            btnNuevo.Size = new Size(101, 48);
+            btnNuevo.Size = new Size(144, 80);
             btnNuevo.TabIndex = 104;
             btnNuevo.UseVisualStyleBackColor = true;
+            btnNuevo.Click += btnNuevo_Click_1;
             // 
             // frmAreaDpto
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(240, 236, 230);
-            ClientSize = new Size(1135, 595);
+            ClientSize = new Size(1621, 992);
             Controls.Add(btnEliminar);
             Controls.Add(btnGuardar);
             Controls.Add(btnNuevo);
             Controls.Add(txtNombreArea);
             Controls.Add(pnlAreaDpto);
-            Controls.Add(dgvUsuarios);
+            Controls.Add(dgvAreaDpto);
             Controls.Add(lblTitulo);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 5, 4, 5);
             Name = "frmAreaDpto";
             Text = "Áreas y Departamentos";
             pnlAreaDpto.ResumeLayout(false);
             pnlAreaDpto.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvUsuarios).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvAreaDpto).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -227,11 +209,8 @@
         private Label lblTitulo;
         private TextBox txtNombreArea;
         private Panel pnlAreaDpto;
-        private TextBox txtMatricula;
-        private DataGridView dgvUsuarios;
-        private DataGridViewTextBoxColumn Column1;
-        private DataGridViewTextBoxColumn Column2;
-        private DataGridViewTextBoxColumn Column3;
+        private TextBox txtArea;
+        private DataGridView dgvAreaDpto;
         private TextBox txtDescripcion;
         private Button btnEliminar;
         private Button btnGuardar;
