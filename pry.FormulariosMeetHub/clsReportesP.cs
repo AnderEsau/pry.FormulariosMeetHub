6b6487d6c73e785bccd4f8596caf5be03b5b5b36
laryb <laryb@LARY>
2026-07-21 18:11:20 -0600

    Realicé codigo en el formulario de reportes parciales

---
+++ pry.FormulariosMeetHub/clsReportesP.cs
@@ -0,0 +1,104 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pry.FormulariosMeetHub
+{
+    internal class clsReportesP
+    {
+        DataTable tabla;
+        MySqlDataAdapter consulta;
+        public DataTable ConsultarReporteSemanal()
+        {
+            tabla = new DataTable();
+            try
+            {
+                clsConexion conexionBD = new clsConexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELECT S.nombre_sala AS Sala, " + // Agarra el nombre de la sala
+                                 "IFNULL(R.id_alumno, R.matricula) AS Responsable, " +
+                                 "R.fecha_reserva AS 'Fecha de Reserva', " +
+                                 "R.fecha_solicitud AS 'Fecha de Formalización' " +
+                                 "FROM tblreserva R " +
+                                 "INNER JOIN tblsalas S ON R.id_sala = S.id_sala;";
+                                 
+
+                    using (consulta = new MySqlDataAdapter(sql, conexion))//CONDICION
+                    {
+                        consulta.Fill(tabla);
+                    }//se libera la consulta
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al consultar alumnos en riesgo:" + ex.Message);
+            }
+            return tabla;
+        }
+
+        public DataTable consultarReporteMensual()
+        {
+            tabla = new DataTable();
+            try
+            {
+                clsConexion conexionBD = new clsConexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELECT S.nombre_sala AS Sala, " + // Agarra el nombre de la sala
+                                 "IFNULL(R.id_alumno, R.matricula) AS Responsable, " +
+                                 "R.fecha_reserva AS 'Fecha de Reserva', " +
+                                 "R.fecha_solicitud AS 'Fecha de Formalización' " +
+                                 "FROM tblreserva R " +
+                                 "INNER JOIN tblsalas S ON R.id_sala = S.id_sala;";
+
+                    using (consulta = new MySqlDataAdapter(sql, conexion))//CONDICION
+                    {
+                        consulta.Fill(tabla);
+                    }//se libera la consulta
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al consultar alumnos y tutores:" + ex.Message);
+            }
+            return tabla;
+        }
+
+        public DataTable consultarRangoFechas(string fechaInicio, string fechaFin)
+        {
+            tabla = new DataTable();
+            try
+            {
+                clsConexion conexionBD = new clsConexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELECT S.nombre_sala AS Sala, " +
+                                 "IFNULL(R.id_alumno, R.matricula) AS Responsable, " +
+                                 "R.fecha_reserva AS 'Fecha de Reserva', " +
+                                 "R.fecha_solicitud AS 'Fecha de Formalización' " +
+                                 "FROM tblreserva R " +
+                                 "INNER JOIN tblsalas S ON R.id_sala = S.id_sala " +
+                                 "WHERE R.fecha_reserva BETWEEN '" + fechaInicio + "' AND '" + fechaFin + "';";
+
+                    using (consulta = new MySqlDataAdapter(sql, conexion))//CONDICION
+                    {
+                        consulta.Fill(tabla);
+                    }//se libera la consulta
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al consultar alumnos y tutores:" + ex.Message);
+            }
+            return tabla;
+        }
+    }
+}
--- pry.FormulariosMeetHub/frmReportesP.cs
+++ pry.FormulariosMeetHub/frmReportesP.cs
@@ -12,9 +12,78 @@ namespace pry.FormulariosMeetHub
 {
     public partial class frmReportesP : Form
     {
+        clsReportesP reportes;
         public frmReportesP()
         {
             InitializeComponent();
+        }
+
+        private void btnVer_Click(object sender, EventArgs e)
+        {
+            reportes = new clsReportesP();
+
+            if (rdbOpcion1.Checked == true)
+            {
+                //llamar la consulta para cargar en el grid 
+                CargarGridReporteSemanal();
+            }
+            else if (rdbOpcion2.Checked == true)
+            {
+                //llamar la consulta para cargar en el grid 
+                CargarGridReporteMensual();
+            }
+            else if (rdbOpcion3.Checked == true)
+            {
+                CargarGridRangoFechas();
+            }
+        }
+
+        public void CargarGridReporteSemanal()
+        {
+            reportes = new clsReportesP();
+            dgvReporte.DataSource = null;
+            dgvReporte.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            try
+            {
+                dgvReporte.DataSource = reportes.ConsultarReporteSemanal();
+            }
+            catch (Exception ex)    
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void CargarGridReporteMensual()
+        {
+            reportes = new clsReportesP();
+            dgvReporte.DataSource = null;
+            dgvReporte.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            try
+            {
+                dgvReporte.DataSource = reportes.consultarReporteMensual();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void CargarGridRangoFechas()
+        {
+            reportes = new clsReportesP();
+            dgvReporte.DataSource = null;
+            dgvReporte.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            try
+            {
+                string inicio = dtpInicio.Value.ToString("yyyy-MM-dd");
+                string fin = dtpFinal.Value.ToString("yyyy-MM-dd");
+                dgvReporte.DataSource = reportes.consultarRangoFechas(inicio, fin);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
